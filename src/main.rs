e8d2dff6c61d818cdb5e6b22d458e996baf3a3dd
Stefan Schinkel <mail@dreeg.org>
2024-05-24 15:51:14 +0200

    proper JSON returns and sketch test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 #[macro_use]
 extern crate rocket;
-
+use rocket::serde::{json::Json, Serialize};
 use rocket::State;
+// private modeules
 mod json;
 use json::Config;
+
+// a struct for basic messages
+#[derive(Serialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    message: String,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/bar")]
-fn bar(cfg: &State<Config>) -> String {
-    format!("Found {} devices", cfg.devices.len())
+#[get("/devices")]
+fn get_devices(cfg: &State<Config>) -> Json<Message> {
+    let msg = Message {
+        message: format!("Found {} devices", cfg.devices.len()),
+    };
+    Json(msg)
 }
 
 #[launch]
@@ -24,5 +35,20 @@ fn rocket() -> _ {
     rocket::build()
         .manage(cfg)
         .mount("/", routes![index])
-        .mount("/", routes![bar])
+        .mount("/", routes![get_devices])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::index)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
 }
