fb198e3bf905f36ad1f019eaa7eabb7eaab3422c
Stefan Schinkel <mail@dreeg.org>
2024-05-26 17:19:43 +0200

    move tests to separate file

--- src/main.rs
+++ src/main.rs
@@ -39,16 +39,4 @@ fn rocket() -> _ {
 }
 
 #[cfg(test)]
-mod test {
-    use super::rocket;
-    use rocket::http::Status;
-    use rocket::local::blocking::Client;
-
-    #[test]
-    fn hello_world() {
-        let client = Client::tracked(rocket()).expect("valid rocket instance");
-        let response = client.get(uri!(super::index)).dispatch();
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().unwrap(), "Hello, world!");
-    }
-}
+mod tests;
